8ea0596749e245a83f46279b9d7035592dee1b99
LimJiNa <jn4624@gmail.com>
2024-03-22 00:51:08 +0900

    서블릿 예외 처리 - 인터셉터 중복 요청 방지

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -1,24 +1,30 @@
 package hello.exception;
 
-import hello.exception.filter.LogFilter;
-import jakarta.servlet.DispatcherType;
-import jakarta.servlet.Filter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
-    public FilterRegistrationBean logFilter() {
-        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-        filterFilterRegistrationBean.setFilter(new LogFilter());
-        filterFilterRegistrationBean.setOrder(1);
-        filterFilterRegistrationBean.addUrlPatterns("/*");
-        // Request, error 호출의 경우 logFilter 호출
-        // default: DispatcherType.REQUEST
-        filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
-        return filterFilterRegistrationBean;
+//    @Bean
+//    public FilterRegistrationBean logFilter() {
+//        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+//        filterFilterRegistrationBean.setFilter(new LogFilter());
+//        filterFilterRegistrationBean.setOrder(1);
+//        filterFilterRegistrationBean.addUrlPatterns("/*");
+//        // Request, error 호출의 경우 logFilter 호출
+//        // default: DispatcherType.REQUEST
+//        filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+//        return filterFilterRegistrationBean;
+//    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                // 인터셉터는 DispatcherType 설정이 없는 대신 excludePathPatterns로 설정
+                .excludePathPatterns("/css/**", "/*.ico", "/error"/*, "/error-page/**"*/);
     }
 }
